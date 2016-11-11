1eaa85b0e15b04dbe9cbe80148ff973be37bcc32
Horovtom on Wochozka-NTB <horovtom@fel.cvut.cz>
2016-11-11 10:20:37 +0100

    Creating structure for parameter accepting

--- test.sh
+++ test.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Hello World"
+# Process arguments:
+while getopts ":cmrh" opt; do
+    	case $opt in
+	c)echo "c parameter entered";
+		;; # Handle -c argument (copy)
+	m)echo "m parameter entered";
+		;; # Handle -m argument (move)
+	r)echo "r parameter entered";
+		;; # Handle -r argument (remove)
+	h)echo "This is help";
+		;; # Handle -h argument
+    	\?)echo "Invalid option: -$OPTARG" >&2;
+		exit 1;
+		;; # Handle error: unknown option or missing required argument.
+	:)echo "Option -$OPTARG requires argument." >&2;
+		exit 1;
+		;; # Handle no-argument to option with argument"
+	esac
+done
+ext1=${@:$OPTIND:1}
+ext2=${@:$OPTIND+1:1}
+echo "ext1: $ext1"
+echo "ext2: $ext2"
